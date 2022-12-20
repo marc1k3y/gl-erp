345f754bbcf3166cf52f97fc5512693c5c072564
marc1k3y <marc1k3y@gmail.com>
2022-12-20 15:05:10 +0300

    swap to web api

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 // local api
-export const api = "http://192.168.1.16/v2/"
-export const fileApi = "http://192.168.1.16/v2/files/download/attachment?fileName="
+// export const api = "http://192.168.1.16/v2/"
+// export const fileApi = "http://192.168.1.16/v2/files/download/attachment?fileName="
 
 // azure api
-// export const api = "https://g-go-farming.azurewebsites.net/v2/"
-// export const fileApi = "https://g-go-farming.azurewebsites.net/v2/files/download/attachment?fileName="
+export const api = "https://g-go-farming.azurewebsites.net/v2/"
+export const fileApi = "https://g-go-farming.azurewebsites.net/v2/files/download/attachment?fileName="
